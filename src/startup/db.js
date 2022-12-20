8570164820daf29d0cbcb78f89d0916c771990a8
mahmudandroid <mnorelhadi@gmail.com>
2022-12-20 18:46:24 +0400

    add db test server

--- src/startup/db.js
+++ src/startup/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 const winston = require("winston");
 
 module.exports = function () {
-  mongoose.connect("mongodb://localhost/eventDB" ,  { useNewUrlParser: true } ).then((result) => {
+  // mongoose.connect("mongodb://localhost/eventDB" ,  { useNewUrlParser: true } ).then((result) => {
+  //   winston.info("Connection to MongoDB .. " + result.Error);
+  // });
+  mongoose.connect("mongodb+srv://mahmoud:6MDW5FMNfmUa4xSj@cluster0.5tjuslb.mongodb.net/test" ,  { useNewUrlParser: true } ).then((result) => {
     winston.info("Connection to MongoDB .. " + result.Error);
   });
 };
