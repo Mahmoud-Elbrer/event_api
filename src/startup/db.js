6ff513be5963c46514ad2743208e0e257f26380d
mahmudandroid <mnorelhadi@gmail.com>
2022-12-21 20:21:42 +0400

    npm ci test server

--- src/startup/db.js
+++ src/startup/db.js
@@ -5,9 +5,16 @@ module.exports = function () {
   // mongoose.connect("mongodb://localhost/eventDB" ,  { useNewUrlParser: true } ).then((result) => {
   //   winston.info("Connection to MongoDB .. " + result.Error);
   // });
-  
+
+  mongoose.connect("mongodb+srv://mahmoud:6MDW5FMNfmUa4xSj@cluster0.5tjuslb.mongodb.net/test" ,  {   useUnifiedTopology: true   } ).then((result) => {
+    winston.info("Connection to MongoDB dddd .. " + result.Error);
+  });
+};
+
+
+/*
   mongoose.connect("mongodb+srv://mahmoud:6MDW5FMNfmUa4xSj@cluster0.5tjuslb.mongodb.net/?retryWrites=true&w=majority" ,  {   useUnifiedTopology: true   } ).then((result) => {
     console.log(result.CastError);
     winston.info("Connection to MongoDB dddd .. " + result.Error);
   });
-};
+*/
