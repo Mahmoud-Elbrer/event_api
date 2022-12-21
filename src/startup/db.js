7a53df0379c05ef86dd56f048cb2b22139a1448a
mahmudandroid <mnorelhadi@gmail.com>
2022-12-21 13:49:54 +0400

    npm ci test server

--- src/startup/db.js
+++ src/startup/db.js
@@ -10,7 +10,7 @@ module.exports = function () {
 
 
 
-  mongoose.connect("mongodb+srv://mahmoud:6MDW5FMNfmUa4xSj@cluster0.5tjuslb.mongodb.net/test" ,  { useUnifiedTopology: true  } ).then((result) => {
+  mongoose.connect("mongodb+srv://mahmoud:6MDW5FMNfmUa4xSj@cluster0.5tjuslb.mongodb.net/test" ,  {useUnifiedTopology: true ,  useUnifiedTopology: true  } ).then((result) => {
     winston.info("Connection to MongoDB .. " + result.Error);
   });
 };
