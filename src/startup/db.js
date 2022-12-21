a5472f48c5dfd0a2e85a6bf8aa83c3aabf36072b
mahmudandroid <mnorelhadi@gmail.com>
2022-12-21 14:25:31 +0400

    remove models test server

--- src/startup/db.js
+++ src/startup/db.js
@@ -6,10 +6,6 @@ module.exports = function () {
   //   winston.info("Connection to MongoDB .. " + result.Error);
   // });
 
-
-
-
-
   mongoose.connect("mongodb+srv://mahmoud:6MDW5FMNfmUa4xSj@cluster0.5tjuslb.mongodb.net/test" ,  {  useNewUrlParser: true, useUnifiedTopology: true   } ).then((result) => {
     winston.info("Connection to MongoDB .. " + result.Error);
   });
