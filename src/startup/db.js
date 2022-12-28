aabb85397fedb93298f7abfb4ba745e592e69159
mahmudandroid <mnorelhadi@gmail.com>
2022-12-28 21:19:08 +0400

    add compnay emirtae

--- src/startup/db.js
+++ src/startup/db.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const winston = require("winston");
 
 module.exports = function () {
-  mongoose.connect("mongodb://localhost/eventDB" ,  { useNewUrlParser: true } ).then((result) => {
-    winston.info("Connection to MongoDB .. " + result.Error);
-  });
-//  mongodb://localhost:27017
-  // mongoose.connect("mongodb+srv://mahmoud:6MDW5FMNfmUa4xSj@cluster0.5tjuslb.mongodb.net/test" ,  {   useUnifiedTopology: true   } ).then((result) => {
-  //   winston.info("Connection to MongoDB dddd .. " + result.Error);
+  // mongoose.connect("mongodb://localhost/eventDB" ,  { useNewUrlParser: true } ).then((result) => {
+  //   winston.info("Connection to MongoDB .. " + result.Error);
   // });
+//  mongodb://localhost:27017
+  mongoose.connect("mongodb+srv://mahmoud:6MDW5FMNfmUa4xSj@cluster0.5tjuslb.mongodb.net/test" ,  {   useUnifiedTopology: true   } ).then((result) => {
+    winston.info("Connection to MongoDB dddd .. " + result.Error);
+  });
 };
