20c23222ee930cdd3a6043ad3775cfa602f3afa1
mahmudandroid <mnorelhadi@gmail.com>
2023-04-06 00:07:12 +0400

    more update

--- src/controller/company_emirate.js
+++ src/controller/company_emirate.js
@@ -1,27 +1,46 @@
 const _ = require("lodash");
 const { CompanyEmirate } = require("../models/company_emirate");
 const { validateAddCompanyEmirate } = require("../validations/validations");
+const { log } = require("util");
 
 exports.getCompanyEmirate = async (req, res, next) => {
+
+console.log('log i am here ');
+console.log('log i am here ');
+
   let companyEmirate = await CompanyEmirate.find({
-    emirate: req.params.emirateId,
+    
   }).populate("company").populate("emirate");
 
-  var newAr = [];
-  for (var attributename in companyEmirate) {
-    if (
-      companyEmirate[attributename]["company"]["typeCompany"] ==
-      req.params.companyType
-    ) {
-      newAr = companyEmirate;
-    }
-  }
 
-  res.status(200).json(newAr);
+  // console.log(companyEmirate);
+
+  // var newAr = [];
+  // for (var attributename in companyEmirate) {
+  //   if (
+  //     companyEmirate[attributename]["company"]["typeCompany"] ==
+  //     req.params.companyType
+  //   ) {
+  //     newAr = companyEmirate;
+  //   }
+  // }
+
+  res.status(200).json(companyEmirate);
   // res.status(200).json(companyEmirate);
 };
 
 exports.addCompanyEmirate = async (req, res, next) => {
+
+
+
+  let sub = await CompanyEmirate.findOne({ company: req.body.company  ,  emirate : req.body.emirate });
+  if (sub)
+    return res.status(400).json({
+      success: false,
+      message: "تم الاضافة مسبقا | Already Added",
+    });
+
+
   const companyEmirate = CompanyEmirate(
     _.pick(req.body, ["company", "emirate"])
   );
