c34c58c013e20bfa8f96a276582f54568b072c51
mahmudandroid <mnorelhadi@gmail.com>
2023-05-10 00:46:10 +0400

    more update cloud2

--- src/startup/db.js
+++ src/startup/db.js
@@ -2,13 +2,6 @@ const mongoose = require("mongoose");
 const winston = require("winston");
 
 module.exports = function () {
-  // server connect
-  // mongoose.connect("mongodb://127.0.0.1:27017/testServerDB" ,  { useNewUrlParser: true } ).then((result) => {
-  //   winston.info("Connection to MongoDB .. " + result.Error);
-  // });
-
-
-  // localhost
   // mongoose.connect("mongodb://localhost/eventDB" ,  { useNewUrlParser: true } ).then((result) => {
   //   winston.info("Connection to MongoDB .. " + result.Error);
   // });
@@ -17,11 +10,7 @@ module.exports = function () {
   // mongodb://localhost:27017
   // cloud mongodb 
 
-  //cloud mongodb 
   mongoose.connect("mongodb+srv://mahmoud:6MDW5FMNfmUa4xSj@cluster0.5tjuslb.mongodb.net/test" ,  {   useUnifiedTopology: true   } ).then((result) => {
     winston.info("Connection to MongoDB .. " + result.Error);
   });
 };
-
-
-// mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2
