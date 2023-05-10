20dfb93e5cd91ebcae3262b4125881aa95cc63de
mahmudandroid <mnorelhadi@gmail.com>
2023-05-10 12:29:50 +0400

    more update cloud2

--- src/startup/db.js
+++ src/startup/db.js
@@ -4,12 +4,14 @@ const winston = require("winston");
 module.exports = function () {
 
 
-  // server db
-  mongoose.connect("mongodb://127.0.0.1:27017/testEventDB" ,  { useNewUrlParser: true } ).then((result) => {
-    winston.info("Connection to MongoDB .. " + result.Error);
-    console.log(result.Error);
-    console.log("mongodb://localhost:27017");
-  });
+
+
+  // // server db
+  // mongoose.connect("mongodb://127.0.0.1:27017/testEventDB" ,  { useNewUrlParser: true } ).then((result) => {
+  //   winston.info("Connection to MongoDB .. " + result.Error);
+  //   console.log(result.Error);
+  //   console.log("mongodb://localhost:27017");
+  // });
 
 
   // mongoose.connect("mongodb://localhost/eventDB" ,  { useNewUrlParser: true } ).then((result) => {
@@ -20,7 +22,7 @@ module.exports = function () {
   // mongodb://localhost:27017
   // cloud mongodb 
 
-  // mongoose.connect("mongodb+srv://mahmoud:6MDW5FMNfmUa4xSj@cluster0.5tjuslb.mongodb.net/test" ,  {   useUnifiedTopology: true   } ).then((result) => {
-  //   winston.info("Connection to MongoDB .. " + result.Error);
-  // });
+  mongoose.connect("mongodb+srv://mahmoud:6MDW5FMNfmUa4xSj@cluster0.5tjuslb.mongodb.net/test" ,  {   useUnifiedTopology: true   } ).then((result) => {
+    winston.info("Connection to MongoDB .. " + result.Error);
+  });
 };
