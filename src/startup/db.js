c59baaf40337dc8fd8fde385f156636b5be1fe40
mahmudandroid <mnorelhadi@gmail.com>
2023-05-13 11:08:45 +0400

    more update cloud2

--- src/startup/db.js
+++ src/startup/db.js
@@ -2,27 +2,34 @@ const mongoose = require("mongoose");
 const winston = require("winston");
 
 module.exports = function () {
+  // server db
+  // mongoose.connect("mongodb://mahmoud:mama121232@213.190.4.53:27017/eventDB",{ useUnifiedTopology: true }).then((result) => {
+  //     winston.info("Connection to MongoDB .. " + result.Error);
+  //   });
 
-
-
-
-  // // server db
-  // mongoose.connect("mongodb://127.0.0.1:27017/testEventDB" ,  { useNewUrlParser: true } ).then((result) => {
-  //   winston.info("Connection to MongoDB .. " + result.Error);
-  //   console.log(result.Error);
-  //   console.log("mongodb://localhost:27017");
-  // });
-
-
+    // localhost
   // mongoose.connect("mongodb://localhost/eventDB" ,  { useNewUrlParser: true } ).then((result) => {
   //   winston.info("Connection to MongoDB .. " + result.Error);
   // });
-  
 
-  // mongodb://localhost:27017
-  // cloud mongodb 
+  // cloud mongodb
+  mongoose.connect("mongodb+srv://mahmoud:6MDW5FMNfmUa4xSj@cluster0.5tjuslb.mongodb.net/test",{ useUnifiedTopology: true }).then((result) => {
+      winston.info("Connection to MongoDB .. " + result.Error);
+    });
 
-  mongoose.connect("mongodb+srv://mahmoud:6MDW5FMNfmUa4xSj@cluster0.5tjuslb.mongodb.net/test" ,  {   useUnifiedTopology: true   } ).then((result) => {
-    winston.info("Connection to MongoDB .. " + result.Error);
-  });
 };
+
+// var MongoClient = require("mongodb").MongoClient,
+//   format = require("util").format;
+
+// MongoClient.connect("mongodb://127.0.0.1:27017/test", function (err, db) {
+//   if (err) throw err;
+
+//   var collection = db.collection("test_insert");
+//   collection.insert({ a: 2 }, function (err, docs) {
+//     collection.count(function (err, count) {
+//       console.log(format("count = %s", count));
+//       db.close();
+//     });
+//   });
+// });
